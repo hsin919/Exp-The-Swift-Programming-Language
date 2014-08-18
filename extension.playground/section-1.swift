843aa1823c8a27b06efe4ab87b5f316e33a41801
nathan <nathanc@oplink.com.tw>
2014-08-18 09:59:18 +0800

    Add protocol & extension sample

--- extension.playground/section-1.swift
+++ extension.playground/section-1.swift
@@ -2,4 +2,29 @@
 
 import UIKit
 
-var str = "Hello, playground"
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
+7.simpleDescription
+var integer:Int = 7
+integer.adjust()
+integer.simpleDescription
+
+extension Double {
+    var absoluteValue: Double {
+        return abs(self)
+    }
+}
+
+var posDouble = -17.9
+posDouble.absoluteValue
--- protocal.playground/section-1.swift
+++ protocal.playground/section-1.swift
@@ -2,4 +2,68 @@
 
 import UIKit
 
-var str = "Hello, playground"
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += "  Now 100% adjusted."
+    }
+}
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+
+enum simpleEnum: ExampleProtocol {
+    case FirstDescription
+    case Second, Third
+    var simpleDescription: String {
+        get{
+            switch self {
+            case .FirstDescription:
+                return "FirstDescritpion"
+            case .Second:
+                return "Second"
+            case .Third:
+                return "Third"
+            default:
+                return "?"
+            }
+        }
+        
+    }
+  
+    mutating func adjust() {
+        switch self {
+        case .FirstDescription:
+            self = Second
+        case .Second:
+            self = Third
+        case .Third:
+            self = FirstDescription
+        }
+    }
+}
+
+var enumObject = simpleEnum.FirstDescription
+
+let cDescription = enumObject.simpleDescription
+
+enumObject.adjust()
+
+let dDescription = enumObject.simpleDescription
+
